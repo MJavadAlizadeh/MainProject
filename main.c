729d8612fec75e09abe9975d8281dea23e079b01
MJavadAlizadeh <MJavad.Aliizadeh@gmail.com>
2025-12-09 02:56:39 +0330

    some changes+printf

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// faze 1 progect
+// Phase 1 Project
 #include <stdio.h>
 
 const int maxx = 100 ;
@@ -6,8 +6,8 @@ const int maxy = 100 ;
 char hunter = 'H' ;
 char runner = 'R' ;
 char block = '.' ;
-char wallv = '|' ;
-char wallh = '_' ;
+char V = '|' ;
+char H = '_' ;
 char light = 'L' ;
 
 int main () {
@@ -15,7 +15,10 @@ int main () {
      int x , y ;
      printf("Please enter map size:\n");
      scanf ("%d %d",&x,&y) ;
-
+     if (x<1 || y<1) {
+          printf("!EROR!\n(invalid size)\n");
+          return 0 ;
+     }
      char map[maxx][maxy] ;
      for (int i=0 ; i<x ; i++ ){
           for(int j=0 ; j<y ; j++){
@@ -32,9 +35,13 @@ int main () {
      int rcount ;
      printf("Please enter the number of runners:\n");
      scanf ("%d",&rcount ) ;
+     while (rcount<1) {
+          printf("!EROR!\n(invalid number of runners)\nPlease enter again:\n");
+          scanf ("%d",&rcount ) ;
+     }
      for(int i=0 ; i<rcount ; i++) {
           int a , b ;
-          printf("Please enter the coordinates of the runner:\n");
+          printf("Please enter the coordinates of the runner(%d):\n",i+1);
           scanf("%d %d",&a,&b) ;
           map[a][b] = runner ;
      }
@@ -42,9 +49,13 @@ int main () {
      int hcount ;
      printf("Please enter the number of hunters:\n");
      scanf ("%d",&hcount) ;
+     while (hcount<1) {
+          printf("!EROR!\n(invalid number of hunters)\nPlease enter again:\n");
+          scanf ("%d",&hcount) ;
+     }
      for(int i=0 ; i<hcount ; i++) {
           int a , b ;
-          printf("Please enter the coordinates of the hunter:\n");
+          printf("Please enter the coordinates of the hunter(%d):\n",i+1);
           scanf("%d %d",&a,&b) ;
           map[a][b] = hunter ;
      }
@@ -52,40 +63,44 @@ int main () {
      int wcount ;
      printf("Please enter the number of walls:\n");
      scanf("%d",&wcount) ;
-          int walh[maxx][maxy];
-     for (int i=0 ; i<x ; i++ ){
+          int wallh[maxx][maxy];
+          int wallv[maxx][maxy];
+     for (int i=0 ; i<x ; i++ ) {
           for(int j=0 ; j<y ; j++) {
-               walh[i][j] = 0 ;
+               wallh[i][j] = 0 ;
           }
-            int walv[maxx][maxy] ;
-            for (int i=0 ; i<x ; i++ ) {
-                 for(int j=0 ; j<y ; j++) {
-                      walv[i][j] = 0 ;
-                 }
-            }
-     for(int i=0 ; i<wcount ; i++) {
-          int a , b ;
-          char c ;
-          printf("Please enter the coordinates of the wall and (H/V):\n");
-          scanf ("%d  %d %c",&a , &b ,&c) ;
-          if (c=='H'||c == 'h') walh[a][b] = 1 ;
-          else walv[a][b] = 1 ;
      }
+          for (int i=0 ; i<x ; i++ ) {
+               for(int j=0 ; j<y ; j++) {
+                    wallv[i][j] = 0 ;
+               }
+          }
 
+               for(int i=0 ; i<wcount ; i++) {
+                    int a , b ;
+                    char c ;
+                    while (1) {
+                         printf("Please enter the coordinates of the wall(%d) and (H/V):\n",i+1);
+                         scanf ("%d  %d %c",&a , &b ,&c) ;
+                         if (c=='H'||c == 'h'||c=='V'||c=='v') break;
+                         printf("!EROR!\n(Character must be H or V. Try again.)\n");
+                    }
+                         if (c=='H'||c == 'h') wallh[a][b] = 1 ;
+                    else wallv[a][b] = 1 ;
+                    }
 for(int i =0 ; i<x ; i++) {
      for(int j=0 ; j<y ; j++){
           printf("%c",map[i][j]) ;
-          if (walv[i][j]==1){
-               printf("%c",wallv) ;
+          if (wallv[i][j]==1){
+               printf("%c",V) ;
           }
           else printf(" ") ;
 
      } printf("\n") ;
           for(int j=0;j<x;j++) {
-               if(walh[i][j] == 1) printf("%c ",wallh);
+               if(wallh[i][j] == 1) printf("%c ",H);
                else printf("  ");
           }
      printf("\n");
-}
 }
 }
